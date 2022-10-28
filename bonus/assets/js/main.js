dc437759455f6b3d6a62f53677c05235c01a57af
giuliagrfo <giuliagrifo1@gmail.com>
2022-10-28 17:36:28 +0200

    bonus

---
+++ bonus/assets/js/main.js
@@ -0,0 +1,45 @@
+
+const {
+    createApp
+} = Vue
+
+createApp({
+    data(){
+        return {
+            addedTask: '',
+            tasks:[
+                {
+                    text: 'Fare la spesa',
+                    done: true
+                },
+                {
+                    text: 'Fare esercizio Boolean',
+                    done: false
+                },
+                {
+                    text: 'Preparare il pranzo',
+                    done: true
+                },
+                {
+                    text: 'Portare il cane al parco',
+                    done: false
+                },
+            ]
+        }
+        
+    }, 
+    methods: {
+        deleteTask(i) {
+            // console.log(i);
+            this.tasks.splice(i, 1)
+        },
+        addTask(){
+            console.log(this.addedTask);
+            this.tasks.unshift(this.addedTask);
+            this.addedTask = {text:''}
+        },
+        disable(){
+           console.log('sto cliccando sul testo');
+        }
+    }
+}).mount('#app')
